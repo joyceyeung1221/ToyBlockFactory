7bc291f2f658140596e3e7adbc5745e14d865712
Joyce Yeung <69616529+joyceyeung1221@users.noreply.github.com>
2020-11-04 18:34:28 +1100

    completed feature-reportHelper (#1)

---
+++ ToyBlockFactory/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ToyBlockFactory
+{
+    public class Customer
+    {
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+
+        public Customer(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
