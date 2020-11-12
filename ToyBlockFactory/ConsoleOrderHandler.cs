175451234e51c12b4c3f26a97f7aeb7381f855d1
Joyce Yeung <69616529+joyceyeung1221@users.noreply.github.com>
2020-11-12 19:14:16 +1100

    Added unit testing for OrderItemsCollections (#6)

---
+++ ToyBlockFactory/ConsoleOrderHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToyBlockFactory
+{
+    public class ConsoleOrderHandler
+    {
+        private IIO _io;
+        private List<OrderItem> _listOfOptions;
+
+        public ConsoleOrderHandler(IIO io, List<OrderItem> listOfOptions)
+        {
+            _io = io;
+            _listOfOptions = listOfOptions;
+        }
+
+        //public Order CreateOrder()
+        //{
+        //}
+    }
+}
